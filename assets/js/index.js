a03d77642f645629d814882361de6b518577283e
VickycPantanalli <victoriapantanalli@gmail.com>
2024-01-09 13:21:00 -0300

    cambios en directorio

---
+++ assets/js/index.js
@@ -0,0 +1,12 @@
+let precio = 400.000
+
+/* precioSpan = document.querySelector(".precio-inicial");
+cantidad = document.querySelector(".cantidad");
+precioSpan.innerHTML = (precioSpan * cantidad)
+
+
+
+
+button onclick="
+cambiounidad = document.querySelector('.cantidad');
+cambiounidad.innerHTML=('1')"; */
