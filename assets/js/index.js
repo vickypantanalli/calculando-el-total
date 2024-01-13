07f7367528b943fc7a89beda37c9a71c2ffd5fef
VickycPantanalli <victoriapantanalli@gmail.com>
2024-01-12 21:46:48 -0300

    padding 0 todos los elementos

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,12 +1,19 @@
-let precio = 400.000
+precioinicial = document.querySelector(".precio-inicial")
+
+cantidad = document.querySelector(".cantidad")
+
+
+
 
 /* precioSpan = document.querySelector(".precio-inicial");
 cantidad = document.querySelector(".cantidad");
 precioSpan.innerHTML = (precioSpan * cantidad)
 
-
-
-
 button onclick="
 cambiounidad = document.querySelector('.cantidad');
 cambiounidad.innerHTML=('1')"; */
+
+/* let precioinicial = document.querySelector(".precio-inicial")
+        let cantidad = document.querySelector(".cantidad")
+        let valortotal = document.querySelector(".valor-total")
+        valortotal.innerHTML = cantidad*precioinicial */
