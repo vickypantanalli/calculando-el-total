ddf3194033ed3349b1c3992990e9eba8b4718a45
VickycPantanalli <victoriapantanalli@gmail.com>
2024-01-13 09:00:12 -0300

    se agregan variables faltantes

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,7 @@
 let valortotal = document.querySelector("#valortotal")
 let cantidad = document.querySelector(".cantidad")
 let precio-inicial = document.querySelector(".precio-inicial")
+let suma = documento.querySelector("#suma")
+let resta = documento.querySelector("#resta")
+
+
